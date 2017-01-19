bb1b332966a63253f2662c3f37a9351eba006c31
tozeimetz <s4tozeim@uni-trier.de>
2017-01-19 18:22:45 +0100

    difficulty

--- Generator0/GeneratorAlgorithm/Handler.py
+++ Generator0/GeneratorAlgorithm/Handler.py
@@ -2,43 +2,74 @@ from Message import *
 from SudokuGenerator import *
 import math
 
+## Note
+## Zur Zeit akzeptieren wir nur Sudokus die mindestes 9 * 9 Felder besitzen.
+## Kleiner macht aus vielerlei Gründen keinen Sinn, hier ist uns aber nur wichtig,
+## dass wir zu beginn eine fixe Zahl an Werten aus dem Sudoku löschen können.
+
 class Handler():
     global reqDictionary
     reqDictionary={
-        #'reqID123': (difficulty, prevState)
+        #'reqID123': (difficulty, finishedState, cleanedNumbers)
     }
 
 
     def handle(msg):
+        # instruction: generate
         if(msg.instruction.startswith("generate")):
             k = int(math.sqrt(len(msg.sudoku)))
             difficulty = msg.instruction[-2:-1]
             sudoku = generateSudoku(generateFilledSudoku(k),k)
-            reqDictionary[msg.requestID] = (difficulty, sudoku)
-            #TODO Felder leeren
+            
+            # initial cleanup (remove 8 numbers)
+            sudoku, cleanedNumbers = emptyFields(sudoku,8)
+            reqDictionary[msg.requestID] = (difficulty, sudoku, cleanedNumbers) 
 
-            #TODO Camel-Nachicht schicken
+            # TODO Camel-Nachicht schicken
 
+        # instruction: solved one
         elif(msg.instruction == "solved: one"):
+            k = int(math.sqrt(len(msg.sudoku)))
             tmpDifficulty = reqDictionary[msg.requestID][0]
+            
             # now we need the number of 'empty' fields
             emptyCounter = 0
-            for l in msg.sudoku:
-                for i in l:
-                    if i == 0:
+            for row in msg.sudoku:
+                for elem in row:
+                    if elem == 0:
                         emptyCounter++
+
+            percentCounter = (emptyCounter * 100) / (k*k)
+                        
             # and check if we are done or if need still need to 'empty' fields
-            if tmpDifficulty == "1" and emptyCounter > 7:
+            if (tmpDifficulty == "1" and percentCounter < 0.7) or (tmpDifficulty == "2" and percentCounter < 0.5)
+                or (tmpDifficulty == "3" and percentCounter < 0.3):
+                # TODO send sudoku to broker (finished for GUI)
+                
+            # remove numbers
+            sudoku, cleanedNumbers = emptyField(sudoku,1)
+            (difficulty, finishedSudoku, oldNumbers) = reqDictionary[msg.requestID]
+            cleanedNumbers = oldNumbers.extend(cleanedNumbers)
+            reqDictionary[msg.requestID] = (difficutly, finishedSudoku, cleanedNumbers)
+            
+            # TODO send to broker (request to solver)
 
-            if tmpDifficulty == "2" and emptyCounter > 10:
-
-            if tmpDifficulty == "3" and emptyCounter > 13:
-
-            print("Test")
+        # instruction: solved many
         elif(msg.instruction == "solved: many"):
-            print("Test")
+            # recover previous state
+            lastNumber = reqDictionary[msg.requestID][2].pop()
+            (difficulty, finishedSudoku, oldNumbers) = reqDictionary[msg.requestID]
+            sudoku = msg.sudoku
+            sudoku[lastNumber[0]][lastNumber[1]] = finishedSudoku[lastNumber[0]][lastNumber[1]]
+
+            # remove numbers
+            sudoku, cleanedNumbers = emptyField(sudoku,1)
+            cleanedNumbers = oldNumbers.extend(cleanedNumbers)
+            reqDictionary[msg.requestID] = (difficutly, finishedSudoku, cleanedNumbers)
+
+            # TODO send to broker (request to solver)
         else:
-            print("Error")
+            print("Some error occured!")
 
     if __name__ == "__main__":
 
--- Generator0/GeneratorAlgorithm/SimplyRun.py
+++ Generator0/GeneratorAlgorithm/SimplyRun.py
@@ -1,6 +1,39 @@
 from SudokuGenerator import *
 
 sudoku = generateInitialSudoku(k=3)
-sudoku = generateSudoku(sudoku,k=3,level=3)
+sudoku, list = emptyField(sudoku,17)
 print(formattedString(sudoku,k=3))
-print(sudoku)
+
+reqDictionary={
+    'ID1': ('diff', 'finishedSudoku', [(88,88),(99,99)])
+}
+
+
+sudoku, cleanedNumbers = emptyField(sudoku,1)
+(difficulty, finishedSudoku, oldNumbers) = reqDictionary['ID1']
+oldNumbers.extend(cleanedNumbers)
+reqDictionary['ID1'] = (difficulty, finishedSudoku, oldNumbers)
+print(oldNumbers)
+sudoku, cleanedNumbers = emptyField(sudoku,1)
+(difficulty, finishedSudoku, oldNumbers) = reqDictionary['ID1']
+oldNumbers.extend(cleanedNumbers)
+reqDictionary['ID1'] = (difficulty, finishedSudoku, oldNumbers)
+print(oldNumbers)
+sudoku, cleanedNumbers = emptyField(sudoku,1)
+(difficulty, finishedSudoku, oldNumbers) = reqDictionary['ID1']
+oldNumbers.extend(cleanedNumbers)
+reqDictionary['ID1'] = (difficulty, finishedSudoku, oldNumbers)
+print(oldNumbers)
+sudoku, cleanedNumbers = emptyField(sudoku,1)
+(difficulty, finishedSudoku, oldNumbers) = reqDictionary['ID1']
+oldNumbers.extend(cleanedNumbers)
+reqDictionary['ID1'] = (difficulty, finishedSudoku, oldNumbers)
+print(oldNumbers)
+sudoku, cleanedNumbers = emptyField(sudoku,1)
+(difficulty, finishedSudoku, oldNumbers) = reqDictionary['ID1']
+oldNumbers.extend(cleanedNumbers)
+reqDictionary['ID1'] = (difficulty, finishedSudoku, oldNumbers)
+print(oldNumbers)
+print(reqDictionary['ID1'])
+print(reqDictionary['ID1'][2].pop())
+
