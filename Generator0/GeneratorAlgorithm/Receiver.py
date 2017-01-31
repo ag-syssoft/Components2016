d1c324a15477dc07ce43fa387b1dc8d57247b954
Marc Schulz-Narres <git@marcsn.de>
2017-01-31 14:44:52 +0100

    Receiver debug output

--- Generator0/GeneratorAlgorithm/Receiver.py
+++ Generator0/GeneratorAlgorithm/Receiver.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/api/message', methods=['POST'])
 def receive():
 	m = Message(request.json['request-id'], request.json['instruction'], request.json['sudoku'], request.json['sender'])
+	print(m.json())
 	return m.json(), 201
 
 if __name__ == '__main__':
